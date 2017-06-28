7a71bb0f45f12c7f5e73b10438e6b6019e580da6
Azeez Taiwo <Azeez.Taiwo@Interswitchng.com>
2017-06-28 21:05:19 +0100

    In progress

---
+++ app/src/main/java/com/zeddysoft/popularmovies/fragments/TrailerFragment.java
@@ -0,0 +1,28 @@
+package com.zeddysoft.popularmovies.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zeddysoft.popularmovies.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TrailerFragment extends Fragment {
+
+
+    public TrailerFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_trailer, container, false);
+    }
+
+}
